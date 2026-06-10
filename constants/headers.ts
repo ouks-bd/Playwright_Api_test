62f9c1f2c5c507cf748baae13ff4619588deb444
obyed <obyed.oss@gmail.com>
2026-06-10 01:56:15 -0400

    crud

--- constants/headers.ts
+++ constants/headers.ts
@@ -1,6 +1,23 @@
+const reqresApiKey = () => process.env.REQRES_API_KEY ?? process.env.API_KEY;
+const reqresEnv = () => process.env.REQRES_ENV ?? process.env.ENV;
+
 export const HEADERS = {
-  JSON: {
-    'Content-Type': 'application/json',
-    'Accept':       'application/json',
+  JSON: () => {
+    const headers: Record<string, string> = {
+      'Content-Type': 'application/json',
+      'Accept':       'application/json',
+    };
+
+    const apiKey = reqresApiKey();
+    if (apiKey) {
+      headers['x-api-key'] = apiKey;
+    }
+
+    const environment = reqresEnv();
+    if (environment) {
+      headers['X-Reqres-Env'] = environment;
+    }
+
+    return headers;
   },
 };
--- pages/BaseAPI.ts
+++ pages/BaseAPI.ts
@@ -1,50 +1,44 @@
 import { APIRequestContext } from '@playwright/test';
-import { HEADERS } from '../constants/headers';
 import { logger } from '../utils/logger';
 
 export class BaseAPI {
   protected request: APIRequestContext;
   protected baseURL: string;
+  protected headers: Record<string, string>;
 
   constructor(request: APIRequestContext) {
-  this.request = request;
-  this.baseURL = `${process.env.BASE_URL ?? 'https://reqres.in'}${process.env.API_VERSION ?? '/api'}`;
-}
+    this.request = request;
+    this.baseURL = `${process.env.BASE_URL ?? 'https://reqres.in'}${process.env.API_VERSION ?? '/api'}`;
+    this.headers = {
+      'Content-Type': 'application/json',
+      'Accept':       'application/json',
+      'x-api-key':    'reqres-free-v1',
+    };
+  }
+  
+
   protected async get(endpoint: string) {
     logger.info(`GET ${this.baseURL}${endpoint}`);
-    return await this.request.get(`${this.baseURL}${endpoint}`, {
-      headers: HEADERS.JSON,
-    });
+    return await this.request.get(`${this.baseURL}${endpoint}`, { headers: this.headers });
   }
 
   protected async post(endpoint: string, body: object) {
     logger.info(`POST ${this.baseURL}${endpoint}`);
-    return await this.request.post(`${this.baseURL}${endpoint}`, {
-      headers: HEADERS.JSON,
-      data: body,
-    });
+    return await this.request.post(`${this.baseURL}${endpoint}`, { headers: this.headers, data: body });
   }
 
   protected async put(endpoint: string, body: object) {
     logger.info(`PUT ${this.baseURL}${endpoint}`);
-    return await this.request.put(`${this.baseURL}${endpoint}`, {
-      headers: HEADERS.JSON,
-      data: body,
-    });
+    return await this.request.put(`${this.baseURL}${endpoint}`, { headers: this.headers, data: body });
   }
 
   protected async patch(endpoint: string, body: object) {
     logger.info(`PATCH ${this.baseURL}${endpoint}`);
-    return await this.request.patch(`${this.baseURL}${endpoint}`, {
-      headers: HEADERS.JSON,
-      data: body,
-    });
+    return await this.request.patch(`${this.baseURL}${endpoint}`, { headers: this.headers, data: body });
   }
 
   protected async delete(endpoint: string) {
     logger.info(`DELETE ${this.baseURL}${endpoint}`);
-    return await this.request.delete(`${this.baseURL}${endpoint}`, {
-      headers: HEADERS.JSON,
-    });
+    return await this.request.delete(`${this.baseURL}${endpoint}`, { headers: this.headers });
   }
 }
